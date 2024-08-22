eb7513abe2c7c3a41312ac9bb85badc12b5bdae5
SauceNameko <sosuke0587@gmail.com>
2024-08-22 20:50:59 +0900

    AddCorsCountermeasure

--- api/src/main.go
+++ api/src/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:8081"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
